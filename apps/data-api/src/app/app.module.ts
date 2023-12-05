0a598ca115b2dcd3aab5c07f2ba17425bfb87021
Luuk Beks <beksluuk@gmail.com>
2023-12-05 14:33:19 +0100

    added mongodb connection to appmodule

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { environment } from '@cycle-gram-web/shared/util-env';
 import { BackendFeaturesUserModule } from '@cycle-gram-web-main/backend/features';
 import { BackendFeaturesBicycleModule } from '@cycle-gram-web-main/backend/features';
 
 @Module({
-  imports: [BackendFeaturesUserModule, BackendFeaturesBicycleModule],
+  imports: [BackendFeaturesUserModule, BackendFeaturesBicycleModule, MongooseModule.forRoot(environment.MONGO_DB_CONNECTION_STRING, {
+    dbName: 'cycle-gram-web',
+    connectionFactory: (connection) => {
+      return connection;
+    },
+    connectionErrorFactory: (error) => {
+      return error;
+    },
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
